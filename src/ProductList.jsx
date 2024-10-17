a309a3731459ba831cfff352ce9f8d2de96d91f5
adriansalazarc <adrian.1009@live.com>
2024-10-16 20:31:20 -0400

    en task3, faltan puntos 7 y 8, pero creo que ya fueron hechos

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -216,26 +216,26 @@ function ProductList() {
         }
     ];
 
-   const styleObj={
-    backgroundColor: '#4CAF50',
-    color: '#fff!important',
-    padding: '15px',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignIems: 'center',
-    fontSize: '20px',
-   }
-   const styleObjUl={
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '1100px',
-   }
-   const styleA={
-    color: 'white',
-    fontSize: '30px',
-    textDecoration: 'none',
-   }
+    const styleObj={
+        backgroundColor: '#4CAF50',
+        color: '#fff!important',
+        padding: '15px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignIems: 'center',
+        fontSize: '20px',
+    }
+    const styleObjUl={
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '1100px',
+    }
+    const styleA={
+        color: 'white',
+        fontSize: '30px',
+        textDecoration: 'none',
+    }
 
     const handleCartClick = (e) => {
         e.preventDefault();
